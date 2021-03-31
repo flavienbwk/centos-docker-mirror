c6aea75c5857a1128149793750e42f6e7b96bf94
flavienbwk <flavien@berwick.fr>
2021-04-01 01:06:17 +0200

    Now only downloading stable branch

--- mirror.sh
+++ mirror.sh
@@ -12,10 +12,13 @@ wget -v "$src/gpg" -P "$dest"
 # Download mirror
 for arch in "x86_64"
 do
-	arch_src="$src/$version/$arch"
-	arch_dest="$dest"
-	mkdir -p "$arch_dest"
-	wget \
-		--mirror -nH --cut-dirs=1 --convert-links --adjust-extension --page-requisites --no-parent \
-		"$arch_src" -P "$arch_dest"
+	for folder in "stable"
+	do
+		arch_src="$src/$version/$arch/$folder"
+		arch_dest="$dest"
+		mkdir -p "$arch_dest"
+		wget \
+			--mirror -nH --cut-dirs=1 --convert-links --adjust-extension --page-requisites --no-parent \
+			"$arch_src" -P "$arch_dest"
+	done
 done
